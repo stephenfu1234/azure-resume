f5e3edcea0e511209ae59266ba7f5f0ea3775c1f
stephenfu1234 <stephenfu1@gmail.com>
2024-02-04 15:58:45 +0800

    Created github workflows

--- frontend/main.js
+++ frontend/main.js
@@ -7,7 +7,7 @@ const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionApi).then(response => {
+    fetch(functionApi).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API");
